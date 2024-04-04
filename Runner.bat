d96629e46fe70c77ff034c27830e8e89e3d68f4d
Nicolas <nico.homer@hotmail.com>
2024-04-04 18:21:35 -0300

    Update Runner.bat

--- Runner.bat
+++ Runner.bat
@@ -1,75 +1,44 @@
-@ECHO OFF
+@echo off
+REM Max Minecraft Server RAM allocation.
+SET SERVER_MAX_MEMORY=4096M
+REM This checks if the server is running. You have to change the port in server.properties also.
+SET SERVER_PORT=25565
+REM This should depend on the size of the world folder. Be careful, set enough to allocate your world folder.
+SET RAM_MAX_WORLD_MEMORY=3G
+REM Value: 300 equals 5 Minutes.
+SET BACKUP_INTERVAL=300
 
-REM	*****************************************************************
-REM	*				By	NicolasG (Improved Batch v2)				*
-REM *							 	🦘								*
-REM	*****************************************************************
+echo  ^_^_^_      ^_             ^_                   ^_
+echo ^| ^_ ^\^_  ^_^| ^|^_ ^_^_^_   ^_^_^_^| ^|  ^_^_ ^_ ^_  ^_ ^_^_^_  ^| ^|^_^_^_ ^_^_^_ 
+echo ^|  ^_^/ ^|^| ^|  ^_^/ ^_ ^\ ^/ ^-^_^) ^| ^/ ^_^` ^| ^|^| ^/ ^-^_^) ^| ^/ ^-^_^) ^-^_^)
+echo ^|^_^|  ^\^_^,^_^|^\^_^_^\^_^_^_^/ ^\^_^_^_^|^_^| ^\^_^_^, ^|^\^_^,^_^\^_^_^_^| ^|^_^\^_^_^_^\^_^_^_^|
+echo                               ^|^_^|
 
-REM Features
-REM		1)	Easy Server setup.
-REM 	2)	Auto Backups on every Startup.
-REM 	3)	Launch arguments for improve Server Performance.
+title Server 1.20.1 (RAM: %SERVER_MAX_MEMORY%)
 
-REM v2(Because 1.16.5 Minecraft Server version now're in use) Path Notes
-REM 	1)	Added new launch arguments.
-REM 	2)	Convert to portable 7z for World folder Backups.
-REM 	3)	Added code for delete old Logs(.log.gz).
-REM 	4)	More recommendations to "Server Setup Guide" Section.
+start "minecraft_server" /B java -Xmx%SERVER_MAX_MEMORY% @libraries/net/minecraftforge/forge/1.20.1-47.2.20/win_args.txt %* nogui
 
-REM	======================================	Server Setup Guide	======================================
-REM	Step 1) Configure next values...
-REM		Define Max RAM(MB)
-set		MAX_RAM=4096
-REM		Define JAR Filename(Example: umommy.jar).
-set		JAR_NAME=forge-1.16.5-36.2.4.jar
-REM		Enable or Disable World Backup on Startup.
-set		backup=true
-REM 	Enable or Disable Delete All old Logs on Startup.
-set 	deletelogs=true
-REM	Step 2) Exec the batch file, A file(eula.txt) will be created on main folder.
-REM	Step 3) Now open eula.txt and change the value from false to true.
-REM	Step 4) Now exec the batch again and wait to server be online and type stop on server console.
-REM	Step 5) Time to setup the server.properties file, open then & change...
-REM														snooper-enabled from true to false
-REM														view-distance 	from 10 to 8
-REM														online-mode 	from true to false
-REM														==========	Optionals	==========
-REM 															allow-flight from false to true(Can causeplayer kick)
-REM																max-players
-REM																motd (Server description)
-REM	==================================================================================================
+:loop
+	netstat -ano | findstr /R "%SERVER_PORT%.*LISTENING" >nul
+	if %errorlevel% equ 0 (
+		echo Creating Backup...
 
-REM	Get Actual Date & Time
-for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%x-%%y-%%z
-for /f "tokens=1,2,3 delims=:. " %%x in ("%time%") do set t=%%x%%y
+		echo say Creating Backup...&echo(
 
-title Minecraft Server (RAM Allocated: %MAX_RAM%MB)
+		"7z.exe" a -ttar -ssw -x!"world\session.lock" "world_%DATE:~-4%%DATE:~-7,2%%DATE:~-10,2%%time:~0,2%%time:~3,2%%time:~6,2%.tar" "world"
 
-REM Delete old Logs
-if %deletelogs% == true (
-	echo Checking for Logs folder...
-	if exist logs (
-		echo Starting to Delete all old Logs...
-		del /s logs\*.log.gz
-		echo Old Logs has been deleted successfully.
-	) else (
-		echo Logs Folder doesnt exist.
+		echo Done!
+		echo say Done!&echo(
+
+		echo Searching old Backups...
+
+		for /f "skip=3 delims=" %%A in ('dir /b /o-d world_*.tar') do (
+			del "%%A"
+			echo Old Backup: %%A was deleted.
+		)
+		
+		echo Done!
 	)
-)
 
-REM	Make a BACKUP of WORLD
-if %backup% == true (
-	echo Checking for World folder...
-	if exist World (
-		echo Starting World Security Backup...
-		"7z.exe" a -tzip "World_%d%_%t%.zip" "world"
-		echo Backup done!.
-	) else (
-		echo World Folder doesnt exist.
-	)
-)
-
-REM	EXEC Jar
-java -Xmx%MAX_RAM%M -Xms%MAX_RAM%M -server -XX:+ScavengeBeforeFullGC -XX:-UseConcMarkSweepGC -XX:-UseParallelGC -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:GCPauseIntervalMillis=150 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -jar %JAR_NAME% nogui
-
-PAUSE
+	timeout /t %BACKUP_INTERVAL% >nul
+goto loop
